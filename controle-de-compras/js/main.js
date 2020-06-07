ab6dfa3b202c8a35bd8806724fd296bba7587f09
Roger Rezende <roger.rezendegmail.com>
2020-06-07 13:47:11 -0300

    formatando a descrição e o valor dos produtos da compra

--- controle-de-compras/js/main.js
+++ controle-de-compras/js/main.js
@@ -21,10 +21,31 @@ function setLista(lista){
 	var tabela = '<thead><tr><th scope="col">Descrição</th><th scope="col">Quantidade</th><th scope="col">Valor</th><th scope="col">Ação</th></tr></thead><tbody>';
 	//laço de repetição que irá popular a tabela
 	for(var key in lista){
-		tabela += '<tr><th scope="row">' + lista[key].descricao + '</th><td>' + lista[key].quantidade + '</td><td>' + lista[key].valor + '</td><td>Editar | Deletar</td></tr>';
+		tabela += '<tr><th scope="row">' + formatarDescricao(lista[key].descricao) + '</th><td>' + lista[key].quantidade + '</td><td>' + formatarValor(lista[key].valor) + '</td><td>Editar | Deletar</td></tr>';
 	}
 	tabela += '</tbody>';
 	document.getElementById('listaTabela').innerHTML = tabela;
+}
+//função que irá formatar as descrições
+function formatarDescricao(descricao){
+	//irá deixar a descrição toda com letras minúsculas
+	var string = descricao.toLowerCase();
+	//irá deixar a primeira letra maiúscula e concatenar com o resto da descrição
+	string = string.charAt(0).toUpperCase() + string.slice(1);
+	//retorna a descrição alterada
+	return string
+}
+
+//função que irá formatar os valores
+function formatarValor(valor){
+	//irá transformar o valor em float e fixo duas casas decimais
+	var string = parseFloat(valor).toFixed(2) + "";
+	//irá alterar o ponto por vírgula
+	string = string.replace(".", ",");
+	//irá inserir o R$ a frente do valor
+	string = "R$ " + string;
+	//retorna o valor alterado
+	return string
 }
 
 //utilizando a função setLista
